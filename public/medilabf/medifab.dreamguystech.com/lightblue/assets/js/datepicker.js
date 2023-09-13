9b5883e0c065cd127e82b89d8242bde15f5e1c8f
theresiaherrich <124873369+theresiaherrich@users.noreply.github.com>
2023-09-13 13:12:23 +0700

    template admin dan user
    okay tolongg

---
+++ public/medilabf/medifab.dreamguystech.com/lightblue/assets/js/datepicker.js
@@ -0,0 +1,15 @@
+/*
+Author       : Dreamguys
+Template Name: Medifab - Health & Medical HTML Template
+Version      : 1.3
+*/
+
+$(document).ready(function() {	
+	
+	// Datepicker
+	$('#datepicker').datepicker({  
+		autoclose: true,
+		todayHighlight: true,
+	});
+	
+});
